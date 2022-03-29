461ce612bbee54ba41efeb674c6fce638a23f9ed
fizabells22 <fizabella.ru@gmail.com>
2022-03-29 11:26:16 +0700

    publish to github

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_database_sqlite/Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tambahkan Item',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Home(),
+    );
+  }
+}
